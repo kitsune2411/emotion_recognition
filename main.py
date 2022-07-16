d7f95d7d2d9e9bcd7682eb89c6b359d1f3c668f6
Agung awan <0kitsune24@gmail.com>
2022-07-16 11:50:46 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import csv
+import boto3
+
+with open('credential.csv', 'r') as input:
+    next(input)
+    reader = csv.reader(input)
+    for line in reader:
+        access_key_id = line[0]
+        secret_access_key = line[1]
+
+photo = 'women-1.jpg'
+
+client = boto3.client('rekognition',
+                      region_name = 'ap-southeast-1',
+                      aws_access_key_id = access_key_id,
+                      aws_secret_access_key = secret_access_key)
+
+with open(photo, 'rb') as source_img:
+    source_bytes = source_img.read()
+
+response = client.detect_faces(Image={'Bytes':source_bytes}, Attributes=['ALL'])
+
+print(response)
+
